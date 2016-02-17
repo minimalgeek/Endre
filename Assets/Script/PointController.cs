1de9d6e436d9f5d380cf3c1a203146a3dcbcbc74
minimalgeek <minimalgeek@gmail.com>
2016-02-17 23:22:29 +0100

    HUD

---
+++ Assets/Script/PointController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour {
+
+    public Text pointText;
+    private float points;
+
+	void Start () {
+        points = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        points += Time.deltaTime * 100;
+	}
+
+    void OnGUI()
+    {
+        pointText.text = ((int)points).ToString();
+    }
+}
