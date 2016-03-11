e744bfd4ff356ce539acadbf123eb283f0cc4913
minimalgeek <minimalgeek@gmail.com>
2016-03-11 22:02:12 +0100

    Button Fade In

---
+++ Assets/Script/Main/ButtonLateEnabler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonLateEnabler : MonoBehaviour {
+
+    public float sec = 1f;
+    public GameObject parentPanel;
+
+    private Button btn;
+
+    void Start()
+    {
+        btn = gameObject.GetComponent<Button>();
+        btn.interactable = false;
+    }
+
+    void Update() {
+        if (parentPanel.activeInHierarchy)
+        {
+            StartCoroutine(LateCall());
+        }
+    }
+
+    IEnumerator LateCall()
+    {
+        yield return new WaitForSeconds(sec);
+        btn.interactable = true;
+    }
+}
